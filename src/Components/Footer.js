8bfec6aa1800441e58ebadbb9d993c2ffaffdd7b
SrushtiKhandelwal <SrushtiKhandelwal@users.noreply.github.com>
2025-02-12 12:00:00 +0530

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,30 @@
+// Components/Footer.js
+import { FaHeart } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        marginTop: 40,
+        padding: "16px 24px",
+        textAlign: "center",
+        justifyContent: "center",
+        backgroundColor: "var(--nav-bg-color)",
+        color: "var(--text-color)",
+        boxShadow: "0 -2px 6px var(--nav-shadow-color)",
+        fontSize: 18,
+        fontWeight: 500,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+     
+
+     
+    </footer>
+  );
+};
+
+export default Footer;
